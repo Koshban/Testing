07443ecf66ded737ea64231accb3ca711da58c44
Kaushik Banerjee <96236630+Koshban@users.noreply.github.com>
2024-12-02 12:53:15 +0800

    BTREE

---
+++ TaroDSA/BinaryTree/max_root_to_lead_path.py
@@ -0,0 +1,74 @@
+'''
+Write a function, max_path_sum, that takes in the root of a binary tree that contains number values. The function should return the maximum sum of any root to leaf path within the tree.
+
+You may assume that the input tree is non-empty.
+'''
+from BinaryTreeClass import Node
+
+# Iterrative solution
+def max_path_sum(root) -> int:
+    maxpathsum = float('-inf')
+    if root is None:
+        return maxpathsum
+    stack = [root]
+    while stack:
+        current = stack.pop()
+        maxpathsum = max(maxpathsum, current.val )
+
+        if current.left is not None:
+            stack.append(current.left)
+
+        if current.right is not None:
+            stack.append(current.right)
+    
+    return maxpathsum
+
+# Recursive solution
+# def max_path_sum(root) -> int:
+#     if root is None:
+#         return 0
+    
+#     return root.val + max(max_path_sum(root.left), max_path_sum(root.right))
+
+
+if __name__ == "__main__":
+    a = Node(3)
+    b = Node(11)
+    c = Node(4)
+    d = Node(4)
+    e = Node(-2)
+    f = Node(1)
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f
+    #      3
+    #    /   \
+    #   11    4
+    #  / \     \
+    # 4   -2    1
+    print(max_path_sum(a)) # -> 18
+
+
+    a = Node(5)
+    b = Node(11)
+    c = Node(54)
+    d = Node(20)
+    e = Node(15)
+    f = Node(1)
+    g = Node(3)
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    e.left = f
+    e.right = g
+    #       5
+    #     /   \
+    #    11   54
+    #  /   \
+    # 20   15
+    #      / \
+    #     1   3
+    print(max_path_sum(a)) # -> 59
---
+++ TaroDSA/BinaryTree/tree_min_value.py
@@ -0,0 +1,120 @@
+'''
+Write a function, tree_min_value, that takes in the root of a binary tree that contains number values.
+
+The function should return the minimum value within the tree.
+
+You may assume that the input tree is non-empty.
+'''
+from BinaryTreeClass import Node
+from collections import deque
+
+''' Using DFS iteratively'''
+
+def tree_min_value_iteratively(root) -> int:
+    if root is None:
+        return 0
+    stack, minval = [], float('inf')
+    stack = [ root]
+    while stack:
+        current = stack.pop()
+        minval = min(current.val, minval)
+        if current.left is not None:
+            stack.append(current.left)
+        
+        if current.right is not None:
+            stack.append(current.right)
+    return minval
+
+
+''' Using DFS recursively'''
+
+def tree_min_value_recursively(root) -> int:
+    minval = float('inf')
+    if root is None:
+        return minval
+
+    return min(root.val, tree_min_value_recursively(root=root.left), tree_min_value_recursively(root=root.right))
+
+''' Using BFS '''
+
+def tree_min_value_BFS(root) -> int:
+    if root is None:
+        return 0
+    minval = float('inf')
+    queue = deque([ root ])
+    while queue:
+        current = queue.popleft()
+        minval = min(minval, current.val)
+        if current.left is not None:
+            queue.append(current.left)
+        
+        if current.right is not None:
+            queue.append(current.right)
+    return minval
+
+
+
+    
+
+
+if __name__ == "__main__":
+    a = Node(3)
+    b = Node(11)
+    c = Node(4)
+    d = Node(4)
+    e = Node(-2)
+    f = Node(1)
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f
+    #      3
+    #    /   \
+    #   11    4
+    #  / \     \
+    # 4   -2    1
+    print(tree_min_value(a)) # -> -2
+
+    a = Node(5)
+    b = Node(11)
+    c = Node(3)
+    d = Node(4)
+    e = Node(14)
+    f = Node(12)
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f
+    #      5
+    #    /   \
+    #   11    3
+    #  / \     \
+    # 4   14    12
+    print(tree_min_value(a)) # -> 3
+
+    a = Node(-1)
+    b = Node(-6)
+    c = Node(-5)
+    d = Node(-3)
+    e = Node(-4)
+    f = Node(-13)
+    g = Node(-2)
+    h = Node(-2)
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f
+    e.left = g
+    f.right = h
+    #        -1
+    #      /   \
+    #    -6    -5
+    #   /  \     \
+    # -3   -4   -13
+    #      /       \
+    #    -2        -2
+    print(tree_min_value(a)) # -> -13
+
